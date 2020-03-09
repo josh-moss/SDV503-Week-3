f21855b085549ecaad2f5b1d5f3648444ad4c49a
josh <josh-moss@live.nmit.ac.nz>
2020-03-09 13:40:29 +1300

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let x = 10;
-let y =5;
+let y =3;
 
-console.log("sum = ", x % y);
-console.log("sum = ", x / y);
-console.log("sum = ", x * y);
-console.log("sum = ", x - y);
-console.log("sum = ", x ** y);
-console.log("sum = ", x + y);
+console.log("sum = ",x % y);
+console.log("sum = ",x / y);
+console.log("sum = ",x * y);
+console.log("sum = ",x - y);
+console.log("sum = ",x ** y);
+console.log("sum = ",x + y);
