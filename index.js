04496ecfe747807ef4848eaa4d67dc0da50b1557
josh <josh-moss@live.nmit.ac.nz>
2020-03-09 14:01:58 +1300

    pre increments and post increment recieve a value and either display beofre or after the variable returns

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let x = 10;
-let y =3;
+let y = 3;
+let z = 20;
+let c = 30;
+
+
 
 console.log("sum = ",x % y);
 console.log("sum = ",x / y);
@@ -7,3 +11,12 @@ console.log("sum = ",x * y);
 console.log("sum = ",x - y);
 console.log("sum = ",x ** y);
 console.log("sum = ",x + y);
+
+
+console.log("sum =",++z);
+console.log("sum =",++z); //Pre Increment - inceases by one then returns "Z"
+
+console.log("sum =",c);
+console.log("sum =",c++);
+console.log("Sum =",c++); //Post Increment - returns "c" then the increment
+
